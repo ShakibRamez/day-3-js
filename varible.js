b74e4d3162ce0314112047ff382f2c60ecedfe14
ShakibRamez <shakibramiz1@icloud.com>
2025-06-03 11:32:50 +0430

    varibles cont var let and task completed

---
+++ varible.js
@@ -0,0 +1,25 @@
+//the old var keyword
+
+// var age = 18;
+// let name = "stons";
+
+// console.log("$(name) is $(age) years old");
+
+
+//conts cant change the quantity
+
+// const name = "shakib";
+// name = "ahmad";
+// console.log(name);
+
+
+//task
+
+const currentYear = new Date().getFullYear();
+console.log(currentYear);
+
+let userName = "shakb123";
+console.log(userName);
+
+userName = "harry potter";
+console.log(userName);
