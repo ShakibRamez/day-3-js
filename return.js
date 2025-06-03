53ee95a7cafd1c315d503c90e97fdfa7543944ca
ShakibRamez <shakibramiz1@icloud.com>
2025-06-03 11:24:14 +0430

    return function and task

---
+++ return.js
@@ -0,0 +1,31 @@
+// function test(){
+//     return("hello");
+//     return("bye");
+// };
+
+// console.log(test());
+
+// let login = function(password){
+//     if(password === "1234asdf"){
+//         return "Welcome";
+//     }else{
+//         return "Wrong password";
+//     }
+// }
+
+// let result = login("1234asdf");
+// console.log(result);
+
+//task
+
+function password(a){
+    if(a.lentgh > 8){
+        return console.log("you chose $(a)");
+
+    }else{
+        return console.log("password is too short");
+    }
+}
+
+password("shakhnda");
+password("circle121");
