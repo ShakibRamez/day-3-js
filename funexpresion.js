258ab4a70d5c52e7e7d999ff476d98dd2ee14376
ShakibRamez <shakibramiz1@icloud.com>
2025-06-03 11:15:27 +0430

    javascript expresion

---
+++ funexpresion.js
@@ -0,0 +1,32 @@
+// let game = function(){
+//     console.log("zoro");
+//     console.log("bent 10");
+//     console.log("mac quuen");
+//     console.log("spongy bob");
+// }
+
+// let anotherWayToCall = game;
+// anotherWayToCall();
+
+// myFunc();
+
+// let myFunc = function(){
+//     console.log("my function declaration");
+// };
+
+//task
+let colors = function(){
+    console.log("red", "purple", "pink");
+};
+
+colors();
+ 
+let myColors = colors;
+myColors();
+
+function myName(){
+    console.log("shakib");
+};
+
+let name = myName;
+name();
