49908f642870142f8b0ff706cf4a020d178aefab
ShakibRamez <shakibramiz1@icloud.com>
2025-06-03 11:03:44 +0430

    javascript functions

---
+++ functions.js
@@ -0,0 +1,21 @@
+// function game(){
+//     console.log("sonic the hedgehog");
+//     console.log("super mario");
+//     console.log("ben 10");
+// }
+// game();
+
+// let greating = "hello";
+// function game(){
+//     let title = "zelda";
+//     console.log(title , greating);
+// }
+// game();
+// console.log(greating);
+
+
+let btn = document.querySelector('#btn');
+
+btn.addEventListener("click", function(){
+    console.log("sonic");
+});
